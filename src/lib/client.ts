074d7c4924d530da1ab2303f74a1f4a8093da9ab
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-02-23 17:54:31 +0100

    add env variable for base url

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,11 +1,13 @@
 import { HeadscaleServiceApi } from "./client/apis";
 import { Configuration, type FetchAPI } from "./client/runtime";
+import { env } from '$env/dynamic/public'
 
 export * from './client/index';
 
 export const newClient = (fetch: FetchAPI, key: string) => {
+  const base = env.PUBLIC_BASE_URL || "https://headscale.jonavdm.nl";
   const client = new HeadscaleServiceApi(new Configuration({
-    basePath: 'https://headscale.jonavdm.nl',
+    basePath: base,
     fetchApi: fetch,
     headers: {
       Authorization: `Bearer ${key}`
