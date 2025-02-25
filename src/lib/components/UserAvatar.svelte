6ececfaf2a0f49a50c2e131f522f7acf7280d829
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-02-25 21:16:41 +0100

    add profile image

---
+++ src/lib/components/UserAvatar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	const {
+		avatar,
+		name
+	}: {
+		avatar?: string;
+		name?: string;
+	} = $props();
+
+	const initials = $derived.by(() => {
+		const spl = name?.split(' ') ?? [];
+
+		if (spl.length == 1) {
+			return spl[0][0].toUpperCase();
+		}
+
+		return [spl[0], spl[spl.length - 1]]
+			.map((e) => e[0])
+			.join('')
+			.toUpperCase();
+	});
+</script>
+
+{#if avatar && avatar != ''}
+	<img src={avatar} alt={name} class="w-10 rounded-lg" />
+{:else}
+	<div class="bg-secondary flex h-10 w-10 items-center justify-center rounded-lg text-lg font-thin">
+		{initials}
+	</div>
+{/if}
--- src/routes/(app)/users/+page.svelte
+++ src/routes/(app)/users/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import UserAvatar from '$lib/components/UserAvatar.svelte';
 	import type { PageProps } from './$types';
 
 	const { data }: PageProps = $props();
 
 	const users = $derived(
 		data.users.users?.map((user) => ({
-			name: user.displayName == '' ? user.name : `${user.displayName} (${user.name})`,
+			name: user.displayName == '' ? user.name : `${user.displayName}`,
 			email: user.email == '' ? '-' : user.email,
 			raw: user
 		})) ?? []
@@ -16,7 +17,7 @@
 	<table class="table">
 		<thead>
 			<tr>
-				<th class="bg-base-300 rounded-l-box">Name</th>
+				<th class="bg-base-300 rounded-l-box">User</th>
 				<th class="bg-base-300">Email</th>
 				<th class="bg-base-300">ID</th>
 				<th class="bg-base-300 rounded-e-box">Joined on</th>
@@ -25,7 +26,10 @@
 		<tbody>
 			{#each users as user}
 				<tr class="hover:bg-base-200">
-					<td>{user.name}</td>
+					<td class="flex items-center gap-2">
+						<UserAvatar name={user.name} avatar={user.raw.profilePicUrl} />
+						{user.name}
+					</td>
 					<td>{user.email}</td>
 					<td>{user.raw.id}</td>
 					<td>{user.raw.createdAt?.toLocaleDateString()}</td>
